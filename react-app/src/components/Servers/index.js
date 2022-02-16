ebe4bda9374cc74e262a3ab50f3ebaa313956743
suhaylkhan <suhaylmkhan@gmail.com>
2022-02-16 03:01:08 -0600

    users servers can now be listed upon log in

--- react-app/src/components/Servers/index.js
+++ react-app/src/components/Servers/index.js
@@ -1,7 +1,18 @@
+import { useSelector } from 'react-redux';
+
 function Servers() {
+  const servers = useSelector(state => state.servers);
+
   return (
     <>
-      <h1>SERVERS</h1>
+      <h1>User's Servers</h1>
+      {Object.keys(servers).map(serverId => {
+        return (
+        <ul key={serverId}>
+          <li>{servers[serverId].name}</li>
+        </ul>
+      )
+      })}
     </>
   )
 }
