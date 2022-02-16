f02e302e073628ec9355f91ad22d399fc7e1c462
suhaylkhan <suhaylmkhan@gmail.com>
2022-02-16 13:13:25 -0600

    created channel thunk to load server channels in channels component

--- react-app/src/components/Channels/index.js
+++ react-app/src/components/Channels/index.js
@@ -1,4 +1,16 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import { loadChannels } from '../../store/channel';
+
 function Channels() {
+  const dispatch = useDispatch();
+  const { serverId } = useParams();
+
+  useEffect(() => {
+    (async () => await dispatch(loadChannels(serverId)))()
+  }, [dispatch, serverId])
+
   return (
     <>
       <h1>CHANNELS</h1>
--- react-app/src/store/channel.js
+++ react-app/src/store/channel.js
@@ -1,8 +1,33 @@
+const SET_CHANNELS = 'channels/SET_SERVERS';
+
 const initialState = {}
+
+const setChannels = channels => ({
+  type: SET_CHANNELS,
+  payload: channels
+})
+
+export const loadChannels = serverId => async dispatch => {
+  const response = await fetch(`/api/servers/${serverId}/channels`);
+
+  if (response.ok) {
+    const data = await response.json();
+    dispatch(setChannels(data.channels));
+    return;
+  }
+}
 
 export default function reducer(state = initialState, action) {
   let newState = {...state};
   switch (action.type) {
+    case SET_CHANNELS:
+      newState = action.payload.reduce((a, b) => {
+        return {
+          ...a,
+          [b.id]: b
+        }
+      }, {})
+      return newState;
     default:
       return state;
   }
