8d4ee5561e13864304b908f1b30d0ac87730c5a5
suhaylkhan <suhaylmkhan@gmail.com>
2022-02-18 14:16:32 -0600

    created footer component and added some minor styles

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -14,6 +14,7 @@ import { authenticate } from './store/session';
 import EditChannelForm from './components/EditChannelForm';
 import { useServerContext } from './context/ServerContext';
 import { loadChannels } from './store/channel';
+import Footer from './components/Footer';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -73,6 +74,7 @@ function App() {
 
         <Route path='/' exact={true} >
           <HomePage />
+          <Footer />
         </Route>
       </Switch>
     </BrowserRouter>
---
+++ react-app/src/components/Footer/index.js
@@ -0,0 +1,18 @@
+import './Footer.css';
+
+function Footer() {
+  return (
+    <>
+      <div id='footer-container'>
+        <div id='footer-inner-container'>
+          <div className='footer-div-1'>
+            <span>SPACE IS EMPTY.</span>
+            <span>MAKE IT LESS SO.</span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Footer;
