929df53743f61be10de6670c62330e40321aaf79
suhaylkhan <suhaylmkhan@gmail.com>
2022-02-18 19:52:59 -0600

    added more info to homepage component

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -12,19 +12,29 @@ function HomePage() {
   return (
     <>
       <div className='home-img-container'>
-        HOMEPAGE AND WHATNOT
+        <div>
+          SPACE IS EMPTY.
+        </div>
+        <div>
+          It can get lonely travelling through the vastness of space alone.
+        </div>
       </div>
       <div className='home-containers-2'>
-        INFO USE MY SITE PLEASE
+        <div>
+          MAKE IT LESS SO.
+        </div>
+        <div>
+          Create a Solar System and fill it with Planets that suits your needs.
+          Invite your friends to fill the emptiness of space.
+        </div>
       </div>
       <div className='home-containers-1'>
-        INFO USE MY SITE PLEASE
-      </div>
-      <div className='home-containers-2'>
-        INFO USE MY SITE PLEASE
-      </div>
-      <div className='home-containers-1'>
-        INFO USE MY SITE PLEASE
+        <div>
+          CREATE GALAXY-SIZED COMMUNITIES
+        </div>
+        <div>
+          With Nebulae it's easy to start and grow your community.
+        </div>
       </div>
     </>
   )
