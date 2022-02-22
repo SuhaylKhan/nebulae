abe512fd51e3a91f380b75e4f626c28ff2486321
suhaylkhan <suhaylmkhan@gmail.com>
2022-02-21 19:35:32 -0600

    added styles to AddServerForm modal

--- react-app/src/components/AddServerForm/index.js
+++ react-app/src/components/AddServerForm/index.js
@@ -1,12 +1,26 @@
+import './AddServerForm.css';
+
 function AddServerForm({ props }) {
   const { setServerAction } = props;
 
   return (
     <>
-      <div>Ready for adventure?</div>
-      <div>A Solar System is where you and your friends can hang out.</div>
-      <button onClick={() => setServerAction('CREATE')}>Create my own</button>
-      <button onClick={() => setServerAction('JOIN')}>Join via invite</button>
+      <div className='server-form'>
+        <div className='server-form-header'>Ready for adventure?</div>
+        <div className='server-form-subheader'>A Solar System is where you and your friends can hang out.</div>
+        <button
+          className='server-form-button'
+          onClick={() => setServerAction('CREATE')}
+        >
+          Create my own
+        </button>
+        <button
+          className='server-form-button'
+          onClick={() => setServerAction('JOIN')}
+        >
+          Join via invite
+        </button>
+      </div>
     </>
   )
 }
