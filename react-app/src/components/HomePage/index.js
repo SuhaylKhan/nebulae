7d520e48e00deec883924ac67dd2fc6b9ea951ec
suhaylkhan <suhaylmkhan@gmail.com>
2022-02-25 13:00:39 -0600

    added styles to home page

--- react-app/src/components/HomePage/index.js
+++ react-app/src/components/HomePage/index.js
@@ -13,27 +13,33 @@ function HomePage() {
     <>
       <div className='home-img-container'>
         <div>
-          SPACE IS EMPTY.
-        </div>
-        <div>
-          It can get lonely travelling through the vastness of space alone.
-        </div>
-      </div>
-      <div className='home-containers-2'>
-        <div>
-          MAKE IT LESS SO.
-        </div>
-        <div>
-          Create a Solar System and fill it with Planets that suits your needs.
-          Invite your friends to fill the emptiness of space.
+          <div className='home-big-header'>
+            <span>SPACE</span> IS EMPTY
+          </div>
+          <div className='home-small-header'>
+            It can get lonely travelling through the vastness of space alone.
+          </div>
         </div>
       </div>
-      <div className='home-containers-1'>
-        <div>
-          CREATE GALAXY-SIZED COMMUNITIES
+      <div className='home-sub-container'>
+        <div className='home-containers-2'>
+          <div className='home-big-header'>
+            MAKE IT <span>LESS SO</span>
+          </div>
+          <div className='home-small-header'>
+            Create a Solar System and fill it with Planets that suits your needs.
+            Invite your friends to fill the emptiness of space.
+          </div>
         </div>
-        <div>
-          With Nebulae it's easy to start and grow your community.
+      </div>
+      <div className='home-sub-container'>
+        <div className='home-containers-1'>
+          <div className='home-big-header'>
+            CREATE <span>GALAXY-SIZED</span> COMMUNITIES
+          </div>
+          <div className='home-small-header'>
+            With Nebulae it's easy to start and grow your community.
+          </div>
         </div>
       </div>
     </>
