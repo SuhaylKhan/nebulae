3cbdb053c0f622f48ab86111aa8bb734fd3f9aec
suhaylkhan <suhaylmkhan@gmail.com>
2022-02-25 18:09:16 -0600

    added styles to NoServers and NoChannels components

--- react-app/src/components/Channels/NoChannels.js
+++ react-app/src/components/Channels/NoChannels.js
@@ -1,13 +1,17 @@
 function NoChannels() {
   return(
     <>
-      <div>No Planets found</div>
-      <div>
-        Solar Systems aren't really systems if there are no Planets.
-      </div>
-      <div>
-        Get the party started by creating planets or wait for
-        the Solar System admin to do so.
+      <div className="no-servers-container">
+        <div className="no-servers-header">No <span>Planets</span> found</div>
+        <div className="no-servers-inner-text">
+          <div div className='no-servers-flavor-text'>
+            Solar Systems aren't really systems if there are no Planets.
+          </div>
+          <div>
+            Get the party started by creating planets or wait for
+            the Solar System admin to do so.
+          </div>
+        </div>
       </div>
     </>
   )
--- react-app/src/components/Servers/NoServers.js
+++ react-app/src/components/Servers/NoServers.js
@@ -1,14 +1,20 @@
+import './NoServers.css';
+
 function NoServers() {
   return (
     <>
-      <div>No Solar Systems found</div>
-      <div>
-        If you scream in space and no one is around
-        to hear it, did you make a sound?
-      </div>
-      <div>
-        Let's not think about that. Press the ' + '
-        above to join a Solar System or create your own.
+      <div className="no-servers-container">
+        <div className="no-servers-header">No <span>Solar Systems</span> found</div>
+        <div className="no-servers-inner-text">
+          <div className='no-servers-flavor-text'>
+            If you scream in space and no one is around
+            to hear it, did you make a sound?
+          </div>
+          <div>
+            Let's not think about that. Press the ' + '
+            above to join a Solar System or create your own.
+          </div>
+        </div>
       </div>
     </>
   )
